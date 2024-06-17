36fdd7e012e508192dbd5e461e61617b9973d02b
okguidong <101470646+okguidong@users.noreply.github.com>
2024-06-17 16:55:23 +0900

    Update GPS.cpp

--- 센싱&회로/gps_class/GPS.cpp
+++ 센싱&회로/gps_class/GPS.cpp
@@ -1,27 +1,37 @@
 #include "GPS.h"
 
-bool GPS::set() {
-    TinyGPSPlus _gps = TinyGPSPlus();
+bool GPS::set()
+{
     _serial.begin(_baudrate);
-    if(millis() > 5000 &&_gps.charsProcessed() < 10){
+    if (!_serial)
+    {
+        return false;
+    }
+    if (millis() > 5000 && _gps.charsProcessed() < 10)
+    {
         return false;
     }
     return true;
 }
 
-bool GPS::ready(){
-    while(_serial.available()>0){
-        if(_gps.encode(_serial.read()) && _gps.location.isValid()){
-            return true;
+bool GPS::ready()
+{
+    bool isValid = false;
+    while (_serial.available() > 0)
+    {
+        if (_gps.encode(_serial.read()))
+        {
+            if (_gps.location.isValid())
+            {
+                isValid = true;
+            }
         }
-        return false;
     }
+    return isValid;
 }
 
-void GPS::read(float* lat , float* lng) 
+void GPS::read(float *lat, float *lng)
 {
     *lat = _gps.location.lat();
     *lng = _gps.location.lng();
 }
-
-
