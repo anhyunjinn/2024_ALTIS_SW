185130b654605213eb6a7989469f7565179d44f0
okguidong <101470646+okguidong@users.noreply.github.com>
2024-06-17 16:56:27 +0900

    정상작동

--- 센싱&회로/gps_class/main.cpp
+++ 센싱&회로/gps_class/main.cpp
@@ -1,25 +1,31 @@
 #include <Arduino.h>
+#include "TinyGPSPlus.h"
 #include "GPS.h"
 
-GPS gps(Serial2,9600);  // GPS 객체 생성
+GPS gps(Serial3, 9600); // GPS 객체 생성
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  while(!gps.set()){  // GPS 초기화및 상태 확인
-  Serial.println("gps 에러");
+  if (!gps.set())
+  { // GPS 초기화및 상태 확인
+    while (1)
+    {
+      Serial.println("gps 에러");
+      delay(1000);
+    }
   }
 }
 
-void loop() {
-  if (gps.ready()) {  // GPS 데이터 준비 완료 확인
+void loop()
+{
+  if (gps.ready())
+  {
     float latitude, longitude;
-    gps.read(&latitude, &longitude);  // GPS 데이터 읽고 저장
-
-    // 읽은 GPS 데이터 출력
+    gps.read(&latitude, &longitude);
     Serial.print("Latitude: ");
     Serial.println(latitude, 6);
     Serial.print("Longitude: ");
     Serial.println(longitude, 6);
-    Serial.println();
   }
 }
